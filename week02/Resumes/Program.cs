8a74d9bfa5e6d96998b7d48f63d989f9ef729209
TheSergei <squintanilla@byupathway.edu>
2026-01-08 19:22:08 -0600

    resume assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "indeed";
+        job1._jobTitle = "engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2025;
+        
+
+        Job job2 = new Job();
+        job2._company = "apple";
+        job2._jobTitle = "ceo";
+        job2._startYear = 2025;
+        job2._endYear = 2026;
+        
+
+        Resume resume = new Resume();
+        resume._name = "sergio";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
+
 }
